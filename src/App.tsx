c46d36cadc4e88ea54912dbf87d92ea0582fc393
tanonkim <robbins4bos@gmail.com>
2023-07-22 11:26:22 +0900

    Refactor: Add styled-component setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+import { flex } from 'styles';
 
 function App() {
-  return <div>React Test</div>;
+  return <Container>React Test</Container>;
 }
 
 export default App;
+
+const Container = styled.div`
+  ${flex('center', 'center', 'column')}
+  width: 100vw;
+  height: 100vh;
+`;
