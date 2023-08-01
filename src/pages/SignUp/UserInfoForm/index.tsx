b5317492b153ffc5259c9b2318ce28830d8c7ddc
tanonkim <robbins4bos@gmail.com>
2023-08-01 20:39:27 +0900

    Refactor: Divide textinput

--- src/pages/SignUp/UserInfoForm/index.tsx
+++ src/pages/SignUp/UserInfoForm/index.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { colors, font } from 'styles';
 import { emailRegex, passwordRegex } from 'utils/regex';
+import TextInput from './TextInput';
 
 interface UserInfoFormProps {
   signUpInfo: SignUpInfoType;
@@ -12,105 +13,65 @@ function UserInfoForm({ signUpInfo, onChange }: UserInfoFormProps) {
 
   return (
     <>
-      <Conatiner>
-        <Label>이메일</Label>
-        <InputForm
-          type="text"
-          placeholder="예) YourHomeIsMine@yhim.com"
-          className="email"
-          name="email"
-          onChange={onChange}
-        />
+      <TextInput
+        label="이메일"
+        type="text"
+        placeholder="예) YourHomeIsMine@yhim.com"
+        className="email"
+        name="email"
+        onChange={onChange}
+      >
         <Button
           className={emailRegex.test(email) ? 'buttonOn' : 'button'}
           disabled
         >
           O
         </Button>
-      </Conatiner>
-      <Conatiner>
-        <Label>비밀번호</Label>
-        <InputForm
-          type="password"
-          placeholder="영문 대소문자, 숫자, 특수문자를 포함하여 8자리 이상"
-          className="password"
-          name="password"
-          onChange={onChange}
-        />
-      </Conatiner>
-      <Conatiner>
-        <Label>비밀번호 확인</Label>
-        <InputForm
-          type="password"
-          placeholder="비밀번호를 한번 더 입력해주세요"
-          className="passwordCheck"
-          name="passwordCheck"
-          onChange={onChange}
-        />
+      </TextInput>
+      <TextInput
+        label="비밀번호"
+        type="password"
+        placeholder="영문 대소문자, 숫자, 특수문자를 포함하여 8자리 이상"
+        className="password"
+        name="password"
+        onChange={onChange}
+      />
+      <TextInput
+        label="비밀번호 확인"
+        type="password"
+        placeholder="비밀번호를 한번 더 입력해주세요"
+        className="passwordCheck"
+        name="passwordCheck"
+        onChange={onChange}
+      >
         <Button
-          className={
-            passwordRegex.test(password) &&
-            password === passwordCheck &&
-            password.length >= 8
-              ? 'buttonOn'
-              : 'button'
-          }
+          className={emailRegex.test(email) ? 'buttonOn' : 'button'}
           disabled
         >
           O
         </Button>
-      </Conatiner>
-      <Conatiner>
-        <Label>이름</Label>
-        <InputForm
-          type="text"
-          placeholder="이름을 입력해주세요"
-          className="name"
-          name="name"
-          onChange={onChange}
-        />
-      </Conatiner>
-      <BirthConatiner>
-        <Label>생년월일</Label>
-        <BirthForm
-          type="text"
-          placeholder="예) 1993-11-02"
-          name="birthDate"
-          onChange={onChange}
-          value={birthDate}
-        />
-      </BirthConatiner>
+      </TextInput>
+      <TextInput
+        label="이름"
+        type="text"
+        placeholder="이름을 입력해주세요"
+        className="name"
+        name="name"
+        onChange={onChange}
+      />
+
+      <TextInput
+        label="생년월일"
+        type="text"
+        placeholder="예) 1993-11-02"
+        className="birthDate"
+        name="birthDate"
+        onChange={onChange}
+        value={birthDate}
+      />
     </>
   );
 }
-
-const Conatiner = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 3.5fr 1fr;
-  align-items: center;
-  padding: 10px 0px 10px;
-  margin-left: 20px;
-`;
-
-const Label = styled.label`
-  padding: 10px;
-  ${font(15, 400)}
-`;
-
-const InputForm = styled.input`
-  padding: 15px 25px 15px 10px;
-  margin-left: 10px;
-  border: 1px solid ${colors.MEDIUMGRAY};
-  border-radius: 10px;
-
-  ::placeholder {
-    color: ${colors.GRAY};
-  }
-
-  :focus {
-    outline-color: ${colors.PINK};
-  }
-`;
 
 const Button = styled.button`
   width: 60%;
@@ -125,29 +86,6 @@ const Button = styled.button`
   }
   &.button {
     background-color: ${colors.LIGHTGRAY};
-  }
-`;
-
-const BirthConatiner = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 3.5fr 1fr;
-  align-items: center;
-  padding: 10px 0px 10px;
-  margin-left: 20px;
-`;
-
-const BirthForm = styled.input`
-  padding: 15px 25px 15px 10px;
-  margin-left: 10px;
-  border: 1px solid ${colors.MEDIUMGRAY};
-  border-radius: 10px;
-
-  ::placeholder {
-    color: ${colors.GRAY};
-  }
-
-  :focus {
-    outline-color: ${colors.PINK};
   }
 `;
 
