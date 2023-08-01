7c274fae19fbb3ae9da229f128028af3a4c3e8b4
tanonkim <robbins4bos@gmail.com>
2023-08-01 14:11:24 +0900

    Feature: Add signup basic layout

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,44 @@
+import styled from 'styled-components';
+import { colors, flex, font } from 'styles';
+
 function SignUp() {
-  return <></>;
+  return (
+    <Conatiner>
+      <Header>회원가입 및 소셜로그인</Header>
+      <Title>YHIM에 오신 것을 환영합니다.</Title>
+      <SignUpBtn>회원가입</SignUpBtn>
+    </Conatiner>
+  );
 }
+
+const Conatiner = styled.main`
+  ${flex('', 'center', 'column')}
+  border:1px solid ${colors.MEDIUMGRAY};
+  border-radius: 20px;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  padding: 20px;
+  border-bottom: 1px solid ${colors.MEDIUMGRAY};
+  ${font(15, 600)}
+  text-align: center;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  padding: 20px;
+  margin-top: 10px;
+`;
+
+const SignUpBtn = styled.button`
+  width: 60%;
+  margin: 0px 10px 20px 20px;
+  padding: 20px 15px;
+  border-radius: 10px;
+  background-color: ${colors.PINK};
+  color: ${colors.WHITE};
+  ${font(18, 400)}
+`;
 
 export default SignUp;
