2ec750eb2cb547100336e81d98e94240e573557b
tanonkim <robbins4bos@gmail.com>
2023-08-03 15:13:31 +0900

    Feature: Add datepicker calendar

--- src/pages/Detail/Reservation/Calender/index.tsx
+++ src/pages/Detail/Reservation/Calender/index.tsx
@@ -1,9 +1,73 @@
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import { subDays } from 'date-fns';
+import { ko } from 'date-fns/esm/locale';
+import styled from 'styled-components';
+import { colors, flex, font } from 'styles';
+
 interface CalendarProps {
-  setDate: (email: number) => void;
+  setDate: (date: number) => void;
 }
 
 function Calendar({ setDate }: CalendarProps) {
-  return <></>;
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(new Date());
+
+  return (
+    <Container>
+      <DateContainer>
+        <DateTitle>체크인</DateTitle>
+        <DatePicker
+          selected={startDate}
+          onChange={(date: Date | null) => setStartDate(date)}
+          startDate={startDate}
+          minDate={new Date()}
+          locale={ko}
+          showPopperArrow={false}
+          dateFormat="yyyy.MM.dd (eee)"
+          dateFormatCalendar={'yyyy년 MM월'}
+        />
+      </DateContainer>
+      <DateContainer>
+        <DateTitle>체크아웃</DateTitle>
+        <DatePicker
+          selected={endDate}
+          onChange={(date: Date | null) => setStartDate(date)}
+          startDate={subDays(new Date(), -1)}
+          minDate={new Date()}
+          locale={ko}
+          showPopperArrow={false}
+          dateFormat="yyyy.MM.dd (eee)"
+          dateFormatCalendar={'yyyy년 MM월'}
+        />
+      </DateContainer>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  ${flex('', '')};
+  width: 100%;
+`;
+
+const DateContainer = styled.div`
+  width: 200px;
+  padding: 10px;
+  border: 1px solid ${colors.MEDIUMGRAY};
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  :first-child {
+    border-top-right-radius: 0px;
+    border-right: 0px;
+  }
+  :last-child {
+    border-top-left-radius: 0px;
+  }
+`;
+
+const DateTitle = styled.div`
+  ${font(13, 500)};
+  padding: 10px 0px;
+`;
 
 export default Calendar;
