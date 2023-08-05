35ad27b6abbb2448c9ffb2cf594ee374c93723f7
tanonkim <robbins4bos@gmail.com>
2023-08-05 13:56:28 +0900

    Refactor: Add recoil state in roomDetail index

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -10,35 +10,23 @@ import Notice from './ProductNotice';
 import Map from './ProductMap';
 import HostDetailInfo from './ProductHostInfo';
 import Reservation from './Reservation';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import {
+  descriptionState,
+  detailListState,
+  houseRulesState,
+  priceState,
+  roomAmenitiesState,
+  roomImagesState,
+} from 'recoil/roomState';
 
 function Detail() {
-  const [detailList, setDetailList] = useState<ProductDetailType>({
-    name: '',
-    description: '',
-    district: '',
-    neighberhood: '',
-    price: 0,
-    address: '',
-    guests: 0,
-    beds: 0,
-    bedrooms: 0,
-    baths: 0,
-    latitute: 0,
-    longitute: 0,
-    host: '',
-    host_image: '',
-    host_created: '',
-    room_images_url: [],
-    check_in: [],
-    room_amenities: [],
-    check_in_time: '',
-    check_out_time: '',
-    house_rules: [],
-    category: '',
-  });
-
-  const { room_images_url, description, room_amenities, house_rules, price } =
-    detailList;
+  const [detailList, setDetailList] = useRecoilState(detailListState);
+  const roomImages = useRecoilValue(roomImagesState);
+  const description = useRecoilValue(descriptionState);
+  const roomAmenities = useRecoilValue(roomAmenitiesState);
+  const houseRules = useRecoilValue(houseRulesState);
+  const netPrice = useRecoilValue(priceState);
 
   const params = useParams();
 
@@ -53,16 +41,16 @@ function Detail() {
   return (
     <>
       <Header detailList={detailList} />
-      <ProductImage room_images_url={room_images_url} />
+      <ProductImage room_images_url={roomImages} />
       <Container>
         <ProductContainer>
           <ProductInfo detailList={detailList} />
           <ProductDescription>{description}</ProductDescription>
-          <ProductAmenity room_amenities={room_amenities} />
-          <Notice noticeList={house_rules} />
+          <ProductAmenity room_amenities={roomAmenities} />
+          <Notice noticeList={houseRules} />
         </ProductContainer>
         <ReservationContainer>
-          <Reservation netPrice={price} />
+          <Reservation netPrice={netPrice} />
         </ReservationContainer>
       </Container>
       <Map detailList={detailList} />
